71e7bd76f89f8fbb1264fdae9004dda2af71780c
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2024-01-03 00:28:34 +0100

    Fix indentation in 0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,16 +1,16 @@
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
+	const task = 'I prefer const when I can.';
+	return task;
   }
   
   export function getLast() {
-    return ' is okay';
+	return ' is okay';
   }
   
   export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
+	let combination = 'But sometimes let';
+	combination += getLast();
   
-    return combination;
+	return combination;
   }
   
