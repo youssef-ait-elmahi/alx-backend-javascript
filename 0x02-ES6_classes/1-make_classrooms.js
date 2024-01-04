7fe9ddc165e7c8522f0fa2b769e2e4ec8544f894
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2024-01-04 17:54:35 +0100

    Update import statement in 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,7 @@
-import ClassRoom from "./0-classroom.js";
+import ClassRoom from './0-classroom';
 
 function initializeRooms() {
-  return [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34)
-  ];
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
 
 export default initializeRooms;
