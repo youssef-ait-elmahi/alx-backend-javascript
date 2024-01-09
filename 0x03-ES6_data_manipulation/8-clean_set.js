9410cb46f9f4fb3150cae53c330a64c99bf93cca
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2024-01-09 17:49:50 +0100

    Fix string comparison in cleanSet function

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, str) {
-  if (str === undefined || str.length === 0) return "";
+  if (str === undefined || str.length === 0) return '';
   return [...set]
-    .filter((val) => (val ? val.startsWith(str) : ""))
-    .map((val) => (val ? val.slice(str.length) : ""))
-    .join("-");
+    .filter((val) => (val ? val.startsWith(str) : ''))
+    .map((val) => (val ? val.slice(str.length) : ''))
+    .join('-');
 }
